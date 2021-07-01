a34843f06a8b70acf8d4a3ffa5c2e8d5a07a7d66
Leo Auri <code@leoauri.com>
2021-07-01 14:36:57 +0200

    Add tacotron submodule to package

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.1',  # Required
+    version='0.1.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -130,7 +130,7 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=['waveflow', 'waveflow.models'],  # Required
+    packages=['waveflow', 'waveflow.models', 'waveflow.tacotron2_custom'],  # Required
 
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
